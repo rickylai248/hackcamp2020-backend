49966f4349a3574f75898f2454f14b6e4abb1989
serena-ramley <sramley@gmail.com>
2020-12-06 14:43:37 -0800

    Added some missing imports

--- src/main/java/com/example/demo/GraphQLProvider.java
+++ src/main/java/com/example/demo/GraphQLProvider.java
@@ -1,5 +1,23 @@
 package com.example.demo;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
 @Component
 public class GraphQLProvider {
 
