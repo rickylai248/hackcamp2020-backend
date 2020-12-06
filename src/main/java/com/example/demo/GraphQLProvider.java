b8ad6878c0cb9bed6966a02df98477a65346e660
serena-ramley <sramley@gmail.com>
2020-12-06 13:23:24 -0800

    Added graphql provider file

---
+++ src/main/java/com/example/demo/GraphQLProvider.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+@Component
+public class GraphQLProvider {
+
+    @Autowired
+    GraphQLDataFetchers graphQLDataFetchers;
+
+    private GraphQL graphQL;
+
+    @Bean
+    public GraphQL graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private GraphQLSchema buildSchema(String sdl) {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("employeeById", graphQLDataFetchers.getEmployeeByIdDataFetcher()))
+                .type(newTypeWiring("Employee")
+                        .dataFetcher("name", graphQLDataFetchers.getNameDataFetcher()))
+                .build();
+    }
+}
